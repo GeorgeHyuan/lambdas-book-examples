91d4a842ce3deacef0f880fb472b0093f310a99f
GeorgeHyuan <358779737@qq.com>
2019-05-23 17:20:06 +0800

    Initial commit

---
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -0,0 +1,10 @@
+package com.insightfullogic.java8.answers.chapter2;
+
+import javax.swing.text.DateFormatter;
+import java.text.SimpleDateFormat;
+
+public class Question2 {
+
+  public static final ThreadLocal<DateFormatter> formatter =
+      ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+}
